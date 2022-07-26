f266e4fe2b62df1a1b8ddca1b5d0cfb64cf2c14e
Marcos Maximo <marcosvinmaximo@gmail.com>
2022-07-26 03:52:13 -0300

    corrige color numbers init

--- js/initMoney.js
+++ js/initMoney.js
@@ -3,48 +3,46 @@ export default function clickMoney() {
   const money = document.querySelector(".money");
   let savedMoney = +localStorage.getItem("savedMoney");
 
-  const incrementInterval = Math.floor(savedMoney / 23)
+  const incrementInterval = Math.floor(savedMoney / 53);
   let start = 0;
   const anima = setInterval(() => {
     start += incrementInterval;
     money.innerText = start;
-    if(start > savedMoney){
+    if (start > savedMoney) {
       money.innerText = savedMoney.toFixed(2);
       clearInterval(anima);
     }
   }, 50);
 
-  +money.innerText < 0
-    ? (money.style.color = "red")
-    : (money.style.color = "green");
-  
+  savedMoney <= 0 ? (money.style.color = "red") : (money.style.color = "green");
+
   const decrement = document.querySelector(".decrement-button");
   decrement.addEventListener("click", toggleButton);
-  
+
   function toggleButton() {
-    const sinal = document.querySelectorAll('.sinal');
+    const sinal = document.querySelectorAll(".sinal");
     const incrementOrDecrement = decrement.getAttribute("data-action");
     if (incrementOrDecrement === "increment") {
       decrement.setAttribute("data-action", "decrement");
-      decrement.innerText = 'retirando'
+      decrement.innerText = "retirando";
       sinal.forEach((item) => {
-        item.innerText = '-'
-        item.style.color = 'red'
-      })
+        item.innerText = "-";
+        item.style.color = "red";
+      });
     } else {
       decrement.setAttribute("data-action", "increment");
-      decrement.innerText = 'adicionando'
+      decrement.innerText = "adicionando";
       sinal.forEach((item) => {
-        item.innerText = '+'
-        item.style.color = 'green'
-      })
+        item.innerText = "+";
+        item.style.color = "green";
+      });
     }
   }
-  
+
   notes.forEach((item) => {
     item.addEventListener("click", getValue);
   });
-  
+
   function getValue(e) {
     const increOrDecre = decrement.getAttribute("data-action");
     if (increOrDecre === "increment") {
