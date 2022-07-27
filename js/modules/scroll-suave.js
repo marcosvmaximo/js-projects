2efc63b2d08423a33f40dfae5272cb7af2a4f2c7
Marcos Maximo <marcosvinmaximo@gmail.com>
2022-07-27 12:27:49 -0300

    scroll suave refatorado em classe

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,20 +1,31 @@
-export default function initScroll() {
-  const menuLinks = document.querySelectorAll(
-    '[data-menu="suave"] a[href^="#"]'
-  );
-
-  function scrollToSection(event) {
+export default class ScrollSuave{
+  constructor(links, options){
+    this.linksInternos = document.querySelectorAll(links);
+    if(options === undefined){
+      this.options = { behavior: "smooth",block: "start", }
+    } else {
+      this.options = options
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+  
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
-
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+    
+    section.scrollIntoView(this.options);
+  }
+  
+  addLinkEvent(){
+    this.linksInternos.forEach(link => link.addEventListener("click", this.scrollToSection));
   }
 
-  menuLinks.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  init(){
+    if(this.linksInternos.lenght){
+      this.addLinkEvent()
+      return this 
+      // Por que retornar this? pois o retorno da chamada da classe no arquivo principal, resultara em undefined, e caso o usuario queira acessar outra propriedade ou método da classe, não sera possivel se nãi retornamos a classe a chamada.
+    }
+  }
 }
